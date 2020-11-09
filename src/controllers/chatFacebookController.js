7eeea42450090c3324179282a982191b7f9ed614
Douglas A. C <douglas.affc@gmail.com>
2020-11-09 16:51:31 -0300

    Exploring the attachments.

--- src/controllers/chatFacebookController.js
+++ src/controllers/chatFacebookController.js
@@ -75,6 +75,7 @@ function handleMessage(sender_psid, received_message) {
     }
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
+    console.log(received_message.attachments[0]);
     let attachment_url = received_message.attachments[0].payload.url;
     response = {
       "attachment": {
